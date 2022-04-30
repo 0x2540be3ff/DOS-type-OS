f2f8ce1c5d021ae48f1a00651d77e9db4f940c00
_gigabyte <94195465+0x2540be3ff@users.noreply.github.com>
2022-04-30 11:39:51 +0200

    Update and rename run_terminal.py to boot_dos.py

---
+++ boot_dos.py
@@ -0,0 +1,13 @@
+import os; import time #; import sys
+os.system("cls")
+os.system("title KERNEL")
+time.sleep(0.5)
+print("DOS_BOOT")
+time.sleep(0.5)
+print("")
+print("BOOT : [ OK ]")
+time.sleep(0.33)
+os.system("cls")
+#sys.path.insert(1, './DOS')
+import boot
+quit(0)
